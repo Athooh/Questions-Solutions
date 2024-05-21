a4e9b71a542355e363625d00485b1de5c10d95be
Raymond Madara <140048922+Raymond9734@users.noreply.github.com>
2024-05-21 15:05:22 +0300

    add: rays function solution

--- doop.go
+++ doop.go
@@ -87,4 +87,91 @@ func Itoa(n int) string {
 		r = append(r, temps[i])
 	}
 	return string(r)
-}
+ }
+
+//rays solution
+// package main
+
+// import (
+// 	"fmt"
+// 	"os"
+// )
+
+// func main() {
+
+// 	if len(os.Args[1:]) != 3 {
+// 		return
+// 	}
+
+// 	arg1 := os.Args[1]
+// 	operator := os.Args[2]
+// 	arg2 := os.Args[3]
+// 	var num1 int
+// 	var num2 int
+// 	var ans int
+
+// 	for i, v := range arg1 {
+// 		if i == 0 && v == '-' {
+// 			continue
+// 		}
+// 		num := int(v - 48)
+// 		if num > 9 {
+// 			return
+// 		}
+// 		num1 = (num1 * 10) + num
+
+// 	}
+
+// 	if arg1[0] == '-' {
+// 		num1 = -1 * num1
+// 	}
+
+// 	for i, v := range arg2 {
+
+// 		if i == 0 && v == '-' {
+// 			continue
+// 		}
+
+// 		num := int(v - 48)
+
+// 		if num > 9 {
+// 			return
+// 		}
+// 		num2 = (num2 * 10) + num
+
+// 	}
+// 	if arg2[0] == '-' {
+// 		num2 = -1 * num2
+// 	}
+// 	//fmt.Println(num1)
+// 	//fmt.Println(num2)
+
+// 	if (num1 >= 9223372036854775807 || num1 <= -9223372036854775807) || (num2 >= 9223372036854775807 || num2 <= -9223372036854775807) {
+// 		return
+// 	}
+
+// 	if (num1 == 0 || num2 == 0) && operator == "/" {
+// 		fmt.Println("No division by 0")
+// 		return
+// 	} else if (num1 == 0 || num2 == 0) && operator == "%" {
+// 		fmt.Println("No modulo by 0")
+// 		return
+// 	}
+// 	switch operator {
+// 	case "+":
+// 		ans = num1 + num2
+// 	case "-":
+// 		ans = num1 - num2
+// 	case "/":
+// 		ans = num1 / num2
+// 	case "%":
+// 		ans = num1 % num2
+// 	case "*":
+// 		ans = num1 * num2
+// 	default:
+// 		return
+// 	}
+// 	fmt.Println(ans)
+
+// }
+
