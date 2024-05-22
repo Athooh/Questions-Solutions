99dd94ccdef8c7da39a52a2096f7f929c2522535
rcaleb <calebmadara58@gmail.com>
2024-05-22 10:15:57 +0300

    add: tabmult solution with z01.printrune by ray

--- tabmult.go
+++ tabmult.go
@@ -28,27 +28,75 @@
 
 // $
 
+// package main
+
+// import (
+// 	"fmt"
+// 	"os"
+// 	"strconv"
+// )
+
+// func main() {
+// 	if len(os.Args) != 2 {
+// 		return
+// 	}
+
+// 	args := os.Args[1]
+// 	numb, err := strconv.Atoi(args)
+// 	if err != nil || numb <= 0 {
+// 		fmt.Println("Error! Please provide a valid number.")
+// 		return
+// 	}
+
+// 	for i := 1; i <= 9; i++ {
+// 		fmt.Printf("%d x %d = %d\n", i, numb, i*numb)
+// 	}
+// }
+
 package main
 
 import (
-	"fmt"
 	"os"
-	"strconv"
+
+	"github.com/01-edu/z01"
 )
 
 func main() {
 	if len(os.Args) != 2 {
+		z01.PrintRune('\n')
 		return
 	}
 
-	args := os.Args[1]
-	numb, err := strconv.Atoi(args)
-	if err != nil || numb <= 0 {
-		fmt.Println("Error! Please provide a valid number.")
-		return
+	stInt := os.Args[1]
+	var number int
+	var outPut []string
+	for i, v := range stInt {
+		if i == 0 && v == '-' {
+			return
+		}
+		n := int(v - 48)
+		number = ((number * 10) + n)
 	}
 
 	for i := 1; i <= 9; i++ {
-		fmt.Printf("%d x %d = %d\n", i, numb, i*numb)
+		newStr := string(i + 48)
+		res := (i * number)
+		resStr := ""
+
+		for res > 0 {
+			digit := res % 10
+			resStr = string(digit+'0') + resStr
+			res /= 10
+		}
+
+		outPut = append(outPut, newStr+" "+"x"+" "+stInt+" "+"="+" "+resStr)
+
 	}
+	for i := 0; i < len(outPut); i++ {
+		for _, v := range outPut[i] {
+			z01.PrintRune(v)
+		}
+		z01.PrintRune('\n')
+	}
+
 }
