c77fbc8c1ba593039ba835e083e7023a31cccd03
seodhiambo <oathooh@gmail.com>
2024-05-22 11:11:26 +0300

    more solutions

--- foldint/main.go
+++ foldint/main.go
@@ -1,3 +1,47 @@
+// foldint
+// Instructions
+
+// The function should have as parameters a function, f func(int, int) int a slice of integers, slice []int and an int acc int. For each element of the slice, it should apply the arithmetic function, save the result and print it. The function will be tested with our own functions Add, Sub, and Mul.
+// Expected function
+
+// func FoldInt(f func(int, int) int, a []int, n int) {
+
+// }
+
+// Usage
+
+// Here is a possible program to test your function:
+
+// package main
+
+// func main() {
+// 	table := []int{1, 2, 3}
+// 	ac := 93
+// 	FoldInt(Add, table, ac)
+// 	FoldInt(Mul, table, ac)
+// 	FoldInt(Sub, table, ac)
+// 	fmt.Println()
+
+// 	table = []int{0}
+// 	FoldInt(Add, table, ac)
+// 	FoldInt(Mul, table, ac)
+// 	FoldInt(Sub, table, ac)
+// }
+
+// And its output :
+
+// $ go run .
+// 99
+// 558
+// 87
+
+// 93
+// 0
+// 93
+// $
+
+// Ray Soluton
+
 package main
 
 import (
@@ -19,13 +63,12 @@ func main() {
 }
 
 func FoldInt(f func(int, int) int, a []int, n int) {
-
 	for _, v := range a {
 		n = f(n, v)
-
 	}
 	fmt.Println(n)
 }
+
 func Add(a, b int) int {
 	return a + b
 }
@@ -36,4 +79,41 @@ func Sub(a, b int) int {
 
 func Mul(a, b int) int {
 	return a * b
+}
+
+// Seth Solution
+
+func FoldInt(f func(int, int) int, a []int, n int) {
+	sum := 0
+	for _, v := range a {
+		sum += v
+	}
+	n = f(n, sum)
+	fmt.Println(n)
+}
+
+func Add(a, b int) int {
+	return a + b
+}
+
+func Mul(a, b int) int {
+	return a * b
+}
+
+func Sub(a, b int) int {
+	return a - b
+}
+
+func main() {
+	table := []int{1, 2, 3}
+	ac := 93
+	FoldInt(Add, table, ac)
+	FoldInt(Mul, table, ac)
+	FoldInt(Sub, table, ac)
+	fmt.Println()
+
+	table = []int{0}
+	FoldInt(Add, table, ac)
+	FoldInt(Mul, table, ac)
+	FoldInt(Sub, table, ac)
 }
