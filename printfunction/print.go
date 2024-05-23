732e7904a626e2f26178b10640f686b8a3abb65f
hiombima <ombimahillary52@gmail.com>
2024-05-23 16:05:08 +0300

    updates

--- printfunction/print.go
+++ printfunction/print.go
@@ -25,10 +25,38 @@ if data, ok :=s.(string); ok{
 	foo(s)
 }
 }
+<<<<<<< HEAD
 //printusing z01
+=======
+>>>>>>> 931b61d (updates)
 func foo(s string){
 	for _, c := range s {
 		z01.PrintRune(rune(c))
 	}
 	z01.PrintRune('\n')
 }
+<<<<<<< HEAD
+=======
+
+// func Printn(n int) {
+// 	if n==0{
+// 		z01.PrintRune('0')
+// 		z01.PrintRune('\n')
+// 	}
+// 	if n < 0 {
+// 		z01.PrintRune('-')
+// 		n *= -1
+// 	}
+// 	s := ""
+// 	for n > 0 {
+// 		s = string(rune(n%10+'0')) + s
+// 		n = n / 10
+
+// 	}
+// 	print(s)
+// }
+
+// func main (){
+// 	Print(20)
+// }
+>>>>>>> 931b61d (updates)
