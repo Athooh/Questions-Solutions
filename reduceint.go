0a8ae625f5efddd0eec5a54b1d41ed624fbc6d7b
Stella-Achar-Oiro <johndoe@example.com>
2024-05-23 09:50:14 +0300

    reduceint updated

--- reduceint.go
+++ reduceint.go
@@ -62,6 +62,58 @@ func main() {
 	ReduceInt(as, div)
 }
 
+// Stella Solution
+
+func ReduceInt(a []int, f func(int, int) int) {
+	if len(a) == 0 {
+		return
+	}
+
+	q := a[0]
+	for _, char := range a[1:] {
+		q = f(q, char)
+	}
+	r := Itoa(int64(q))
+	for _, char := range r {
+		z01.PrintRune(char)
+	}
+	z01.PrintRune('\n')
+}
+
+func main() {
+	mul := func(acc int, cur int) int {
+		return acc * cur
+	}
+	sum := func(acc int, cur int) int {
+		return acc + cur
+	}
+	div := func(acc int, cur int) int {
+		return acc / cur
+	}
+	as := []int{500, 2}
+	ReduceInt(as, mul)
+	ReduceInt(as, sum)
+	ReduceInt(as, div)
+}
+
+func Itoa(num int64) string {
+	if num == 0 {
+		return "0"
+	}
+	sign := ""
+	if num < 0 {
+		sign = "-"
+		num = -num
+	}
+	var result string
+	for num > 0 {
+		digit := num % 10
+		result = string(rune('0'+digit)) + result
+		num /= 10
+	}
+	return sign + result
+}
+
 // package main
 
 // import "fmt"
