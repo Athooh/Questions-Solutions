3b988623e1ed101bdbaf33296e2a883a7f284630
Seth Athooh <113475699+Athooh@users.noreply.github.com>
2024-05-24 08:01:42 +0300

    Update chunk.go

--- chunk/chunk.go
+++ chunk/chunk.go
@@ -79,23 +79,75 @@ func main() {
 
 // seth solution
 
+package main
+
+import (
+	"github.com/01-edu/z01"
+)
+
 func Chunk(slice []int, size int) {
 	if size == 0 {
 		z01.PrintRune('\n')
 		return
 	}
 
-	var newSlice [][]int
+	var chunkSlice [][]int
 
 	for i := 0; i < len(slice); i += size {
 		end := i + size
 		if end > len(slice) {
 			end = len(slice)
 		}
-		newSlice = append(newSlice, slice[i:end])
+		chunkSlice = append(chunkSlice, slice[i:end])
 	}
-	fmt.Println(newSlice)
+
+	z01.PrintRune('[')
+	for j, c := range chunkSlice {
+		z01.PrintRune('[')
+		for i, v := range c {
+			for _, r := range Itoa(v) {
+				z01.PrintRune(rune(r))
+			}
+			if i < len(c)-1 {
+				z01.PrintRune(' ')
+			}
+		}
+		z01.PrintRune(']')
+		if j < len(chunkSlice)-1 {
+			z01.PrintRune(' ')
+		}
+	}
+	z01.PrintRune(']')
+	z01.PrintRune('\n')
 }
+
+func main() {
+	Chunk([]int{}, 10)
+	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 0)
+	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 3)
+	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 5)
+	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 4)
+}
+
+func Itoa(n int) string {
+	if n == 0 {
+		return "0"
+	}
+
+	if n < 0 {
+		z01.PrintRune('-')
+		n = -n
+	}
+
+	var digits []rune
+	for n > 0 {
+		digit := n % 10
+		digits = append([]rune{rune('0' + digit)}, digits...)
+		n /= 10
+	}
+	return string(digits)
+}
+
 //Rays solution
 
 func Chunk(slice []int, size int) {
