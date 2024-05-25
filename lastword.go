438c2d5737e6356a58492b333c253ede5149631a
Stella Achar Oiro <stella.achar@azubiafrica.org>
2024-05-25 15:40:00 +0300

    Update lastword.go

--- lastword.go
+++ lastword.go
@@ -97,3 +97,36 @@ func main() {
 	}
 	z01.PrintRune('\n')
 }
+
+// Stella Solution 2
+
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		return
+	}
+
+	args, lastword, start := os.Args[1], "", false
+
+	for i := len(args) - 1; i >= 0; i-- {
+		if args[i] != ' ' {
+			start = true
+			lastword = string(args[i]) + lastword
+		} else if start {
+			break
+		}
+	}
+	if len(lastword) > 0 {
+		for _, char := range lastword {
+			z01.PrintRune(char)
+		}
+		z01.PrintRune('\n')
+	}
+}
