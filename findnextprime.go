90ce65379d8ddb933eab09957eaf37e7b3570340
Seth Athooh <113475699+Athooh@users.noreply.github.com>
2024-05-25 16:43:33 +0300

    Update findnextprime.go

--- findnextprime.go
+++ findnextprime.go
@@ -1,12 +1,14 @@
-// findprevprime
+// findnextprime
 // Instructions
 
-// Write a function that returns the first prime number that is equal or inferior to the int passed as parameter.
+// Write a function that returns the first prime number that is equal or superior to the int passed as parameter.
 
-// If there are no primes inferior to the int passed as parameter the function should return 0.
+// The function must be optimized in order to avoid time-outs with the tester.
+
+// (We consider that only positive numbers can be prime numbers)
 // Expected function
 
-// func FindPrevPrime(nb int) int {
+// func FindNextPrime(nb int) int {
 
 // }
 
@@ -16,54 +18,55 @@
 
 // package main
 
-// import "fmt"
+// import (
+// 	"fmt"
+// 	"piscine"
+// )
 
 // func main() {
-// 	fmt.Println(FindPrevPrime(5))
-// 	fmt.Println(FindPrevPrime(4))
+// 	fmt.Println(piscine.FindNextPrime(5))
+// 	fmt.Println(piscine.FindNextPrime(4))
 // }
 
 // And its output :
 
 // $ go run .
 // 5
-// 3
+// 5
 // $
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func FindPrevPrime(nb int) int {
-	var prevPrime int
-	for i:= 2; i <= nb; i++ {
-		if IsPrime(i) {
-			prevPrime = i
-		}
+func FindNextPrime(nb int) int {
+	if nb < 2 {
+		return 0
 	}
-	return prevPrime
+
+	for !IsPrime(nb) {
+		nb++
+	}
+	return nb
 }
 
-func IsPrime(nb int) bool {
-	if nb < 0 || nb == 1 {
+func IsPrime(n int) bool {
+	if n < 2 {
 		return false
 	}
-	for i := 2; i*i <= nb; i++ {
-		if nb%i == 0 {
+
+	for i := 2; i*i <= n; i++ {
+		if n%i == 0 {
 			return false
 		}
-	}
+	} 
 	return true
 }
 
 func main() {
-	fmt.Println(FindPrevPrime(5)) // Output: 5
-	fmt.Println(FindPrevPrime(4)) // Output: 3
-	fmt.Println(FindPrevPrime(-2))
-	fmt.Println(FindPrevPrime(3))
-	fmt.Println(FindPrevPrime(5))
-	fmt.Println(FindPrevPrime(0))
-	fmt.Println(FindPrevPrime(11))
-	fmt.Println(FindPrevPrime(29))
-}
+	fmt.Println(FindNextPrime(5))
+	fmt.Println(FindNextPrime(4))
 
+}
