410225e23fbee94878ef3437388930d509ee841f
Seth Athooh <113475699+Athooh@users.noreply.github.com>
2024-05-25 12:26:59 +0300

    Update lastword.go

--- lastword.go
+++ lastword.go
@@ -65,3 +65,35 @@ func main() {
 	}
 	z01.PrintRune('\n')
 }
+
+
+// Stella solution
+
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	if len(os.Args) != 2 || os.Args[1] == "  " || os.Args[1] == " " {
+		return
+	}
+	args := os.Args[1]
+	lastWord := ""
+	wordStart := false
+	for i := len(args) - 1; i >= 0; i-- {
+		if args[i] != ' ' {
+			wordStart = true
+			lastWord = string(args[i]) + lastWord
+		} else if wordStart {
+			break
+		}
+	}
+	for _, r := range lastWord {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('\n')
+}
