49421e6ad6d0f25c4fad06270c437bb82abc63fe
Seth Athooh <113475699+Athooh@users.noreply.github.com>
2024-06-01 12:52:04 +0300

    Update main.go

--- sortwordarr/main.go
+++ sortwordarr/main.go
@@ -22,3 +22,29 @@ func SortWordArr(a []string) {
 	}
 
 }
+
+// Seth Solution
+
+package main
+
+import (
+	"fmt"
+)
+
+func SortWordArr(a []string) {
+	for i := 0; i <= len(a)-1; i++ {
+		for j := i + 1; j <= len(a)-1; j++ {
+			if a[i] > a[j] {
+				a[i], a[j] = a[j], a[i]
+			}
+		}
+	}
+}
+
+func main() {
+	result := []string{"a", "A", "1", "b", "B", "2", "c", "C", "3"}
+	SortWordArr(result)
+
+	fmt.Println(result)
+}
+
