3c3fa053a208de6d3949d403c5260ee0c5e2496f
rcaleb <calebmadara58@gmail.com>
2024-06-03 15:12:23 +0300

    Add bracket solution by ray

--- ispower0f2/main.go
+++ ispower0f2/main.go
@@ -1,52 +1,83 @@
-// Seth  Solution
 package main
 
 import (
+	"fmt"
 	"os"
-
-	"github.com/01-edu/z01"
 )
 
 func main() {
 	if len(os.Args) != 2 {
 		return
 	}
-	args := os.Args[1]
-	if Atoi(args) <= 0 {
-		return
-	}
-	
-	word := ""
-	if IsPowerOf2(Atoi(args)) {
-		word = "true"
-	} else {
-		word = "false"
+
+	arg := os.Args[1]
+	newArg := 0
+	for _, v := range arg {
+		n := int(v - 48)
+
+		newArg = (newArg * 10) + n
+
 	}
 
-	for _, c := range word {
-		z01.PrintRune(c)
-	}
-	z01.PrintRune('\n')
+	fmt.Println(isPowerof2(newArg))
+
 }
+
+func isPowerof2(newArg int) bool {
+
+	return newArg%2 == 0
+
+}
+
+// Seth  Solution
+// package main
+
+// import (
+// 	"os"
+
+// 	"github.com/01-edu/z01"
+// )
+
+// func main() {
+// 	if len(os.Args) != 2 {
+// 		return
+// 	}
+// 	args := os.Args[1]
+// 	if Atoi(args) <= 0 {
+// 		return
+// 	}
+
+// 	word := ""
+// 	if IsPowerOf2(Atoi(args)) {
+// 		word = "true"
+// 	} else {
+// 		word = "false"
+// 	}
+
+// 	for _, c := range word {
+// 		z01.PrintRune(c)
+// 	}
+// 	z01.PrintRune('\n')
+// }
 
 func IsPowerOf2(num int) bool {
 	return num > 0 && (num&(num-1)) == 0
 }
 
-func Atoi(s string) int {
-	var number int
-	sign := 1
+// func Atoi(s string) int {
+// 	var number int
+// 	sign := 1
 
-	for idx, char := range s {
-		if char == '-' && idx == 0 {
-			sign = -1
-		} else if char == '+' && idx == 0 {
-			sign = 1
-		} else if char >= '0' && char <= '9' {
-			number = number * 10 + int(char -'0')
-		} else {
-			return 0
-		}
-	}
-	return number * sign
-}
+// 	for idx, char := range s {
+// 		if char == '-' && idx == 0 {
+// 			sign = -1
+// 		} else if char == '+' && idx == 0 {
+// 			sign = 1
+// 		} else if char >= '0' && char <= '9' {
+// 			number = number * 10 + int(char -'0')
+// 		} else {
+// 			return 0
+// 		}
+// 	}
+// 	return number * sign
+// }
