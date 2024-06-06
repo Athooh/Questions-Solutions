d302d5746fa5da1976b15af480c79d515d4f9d63
Seth Athooh <113475699+Athooh@users.noreply.github.com>
2024-06-06 15:39:58 +0300

    Update union.go

--- union.go
+++ union.go
@@ -36,17 +36,12 @@ func main() {
 	args := os.Args[1:]
 
 	union := make(map[rune]bool)
-	unionStr := ""
 
 	for _, c := range args[0] + args[1] {
 		if !union[c] { // checks if the letter is not in the union map yet 
-			unionStr += string(c)
+			z01.PrintRune(c)
 			union[c] = true // ones the letter is added the value becomes true
 		}
-	}
-
-	for _, c := range unionStr {
-		z01.PrintRune(c)
 	}
 	z01.PrintRune('\n')
 }
